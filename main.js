bea378a956896e3cd35b1d7f8a069dcffc7dc323
hkkong <konguuu@gmail.com>
2020-08-26 15:56:01 +0900

    mixin test

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-Vue.component('my-component', {
-    template: '<div>\
-        <p>템플릿1</p>\
-        <p>템플릿22</p>\
-        </div>'
+var mixin = {
+    created: function() {
+        this.hello()
+    },
+    methods: {
+        hello: function() {
+            console.log('hello from mixin!!')
+        }
+    }
+}
+
+//컴포넌트 A
+Vue.component('my-component-a', {
+        mixins: [mixin], //믹스인 등록하기   
+        template: '<div class="my-component-a">component A</div>'
+    })
+    //컴포넌트 B
+Vue.component('my-component-b', {
+    mixins: [mixin], //믹스인 등록하기
+    template: '<div class="my-component-b">component B</div>'
 })
 
 new Vue({
     el: '#app',
     data: {
-        name: '슬라임',
-        hp: 100
+        componentTypes: ['my-component-a', 'my-component-b'],
+        current: 0
+    },
+    computed: {
+        component: function() {
+            //current와 일치하는 index의 컴포넌트를 사용
+            return this.componentTypes[this.current]
+        }
     }
 })
