8647a2c80bf736c9f43af474ed14822d25bc19e7
hkkong <konguuu@gmail.com>
2020-08-26 16:10:13 +0900

    kepp alive test

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
-var mixin = {
-    created: function() {
-        this.hello()
-    },
-    methods: {
-        hello: function() {
-            console.log('hello from mixin!!')
-        }
-    }
-}
+//메시지 목록 전용 컴포넌트
+Vue.component('comp-board', {
+    template: '<div>Message Board</div>',
 
-//컴포넌트 A
-Vue.component('my-component-a', {
-        mixins: [mixin], //믹스인 등록하기   
-        template: '<div class="my-component-a">component A</div>'
-    })
-    //컴포넌트 B
-Vue.component('my-component-b', {
-    mixins: [mixin], //믹스인 등록하기
-    template: '<div class="my-component-b">component B</div>'
+})
+
+Vue.component('comp-form', {
+    template: '<div>Form<textarea v-model="message"></textarea></div>',
+    data: function() {
+        return { message: 'kongtest' }
+    }
 })
 
 new Vue({
     el: '#app',
     data: {
-        componentTypes: ['my-component-a', 'my-component-b'],
-        current: 0
-    },
-    computed: {
-        component: function() {
-            //current와 일치하는 index의 컴포넌트를 사용
-            return this.componentTypes[this.current]
-        }
+        current: 'comp-board' //동적으로 변경하기
     }
 })
