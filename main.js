ea6cc37dee5913502c4a36e95557adcacba9839b
hkkong <konguuu@gmail.com>
2020-08-27 09:30:09 +0900

    css testing

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-//메시지 목록 전용 컴포넌트
-Vue.component('comp-board', {
-    template: '<div>Message Board</div>',
-
-})
-
-Vue.component('comp-form', {
-    template: '<div>Form<textarea v-model="message"></textarea></div>',
-    data: function() {
-        return { message: 'kongtest' }
-    }
-})
-
 new Vue({
     el: '#app',
     data: {
-        current: 'comp-board' //동적으로 변경하기
+        show: true
     }
 })
